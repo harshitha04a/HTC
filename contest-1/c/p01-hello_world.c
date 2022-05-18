73541bdc9b312ad5817aae5c407b3c59a3a0d412
Nireesha7979 <81856769+Nireesha7979@users.noreply.github.com>
2022-05-18 19:59:23 +0530

    Update and rename p1-hello_world.c to p01-hello_world.c

---
+++ contest-1/c/p01-hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Welcome to Hunt the code.\n");
+    printf("Hello Gvpce!");
+    return 0;
+}
