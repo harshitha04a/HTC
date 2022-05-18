b25e7bf2ba1d1faf2fbc1193181d4e5f22d7457d
yaswanth <yaswanth14333@gmail.com>
2022-05-18 19:06:58 +0530

    Update p1-hello_world.cpp

---
+++ contest-1/c++/p1-hello_world.cpp
@@ -0,0 +1,12 @@
+//Write a program to print the string Welcome to Hunt the code. followed by Hello Gvpce! .
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    cout<<"Welcome to Hunt the code.\nHello Gvpce!";
+    return 0;
+}
