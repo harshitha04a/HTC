507005e00d212f2185825e4a0909ab312343d178
yaswanth <yaswanth14333@gmail.com>
2022-05-18 19:09:54 +0530

    Create p3-area_of_circle.cpp

---
+++ contest-1/c++/p3-area_of_circle.cpp
@@ -0,0 +1,15 @@
+// Given radius of circle find out area (consider pi value as 3.14).
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int r;
+    cin>>r;
+    int ans = 3.14*r*r;
+    cout<<ans;
+    return 0;
+}
