ca18e55f3d3f205ab620317ed65490f7bf15cd7f
yaswanth <yaswanth14333@gmail.com>
2022-05-18 19:26:36 +0530

    Create p11-ascii_values.cpp

---
+++ contest-1/c++/p11-ascii_values.cpp
@@ -0,0 +1,14 @@
+// Write a program to take a character from the input and print its ASCII value as the output.
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    char arr;
+    cin>>arr;
+    printf("%d", arr);
+    return 0;
+}
