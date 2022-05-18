879530fe24682ef53ec8a7f03f6987a815c3ee9d
Nireesha7979 <81856769+Nireesha7979@users.noreply.github.com>
2022-05-18 20:08:45 +0530

    Create p03-area_of_circle.c

---
+++ contest-1/c/p03-area_of_circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int r,a;
+    scanf("%d",&r);
+    a = 3.14*(r*r);
+    printf("%d",a);
+    return 0;
+}
